303f5f2f645580921b69374d01c7e560549d5ae4
James L <xell12686@gmail.com>
2022-02-02 04:54:31 +0800

    Success and Failure Callback Functions with es6 syntax

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-import App from "./App";
+// import App from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
+
+// const { checkInventory } = require('./library.js');
+    // refactor above line to es6 syntax
+import checkInventory from "./library.js";
+
+console.clear();
+console.log('init app...');
+
+
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+// Write your code below:
+const handleSuccess = (resolvedVal) => {
+	console.log(resolvedVal);
+}
+const handleFailure = (failedVal) => {
+	console.log(failedVal);
+}
+
+checkInventory(order).then(handleSuccess, handleFailure);
